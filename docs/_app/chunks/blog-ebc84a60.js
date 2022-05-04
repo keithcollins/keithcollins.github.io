564f4e5ec9dfa121f718d7ab772ccfb45a531135
Keith Collins <keithcollins01@gmail.com>
2022-05-04 15:02:31 -0400

    init

---
+++ docs/_app/chunks/blog-ebc84a60.js
@@ -0,0 +1,5 @@
+import{S as d,i as l,s as r,e as u,t as p,c,a as h,h as m,d as i,g as f,F as g,I as o}from"./vendor-15527e3e.js";function v(s){let t,n;return{c(){t=u("p"),n=p(`This is where I post notes about development. So far it has been limited to
+my exploration of Unity and game dev, but in the future it could include thoughts
+on JavaScript and dataviz.`)},l(e){t=c(e,"P",{});var a=h(t);n=m(a,`This is where I post notes about development. So far it has been limited to
+my exploration of Unity and game dev, but in the future it could include thoughts
+on JavaScript and dataviz.`),a.forEach(i)},m(e,a){f(e,t,a),g(t,n)},p:o,i:o,o,d(e){e&&i(t)}}}const b={title:"Blog",slug:"blog",date:"2022-01-26",type:"page"};class x extends d{constructor(t){super();l(this,t,null,v,r,{})}}export{x as default,b as metadata};
